e71d2d6663507f90abc3a7898678938d2856f050
Hoàng Minh Tài <hoangminhtai2810@gmail.com>
2022-09-14 09:29:44 +0700

    added admin pages

---
+++ music_app/src/main/java/com/oracle/registeration/RegisterServlet.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.oracle.registeration;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author hoang
+ */
+@WebServlet(urlPatterns = "Register")
+public class RegisterServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+   
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        String email = request.getParameter("email");
+        String email_confirm = request.getParameter("email_confirm");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        request.setAttribute("show_name", username);
+        request.getRequestDispatcher("index.html").forward(request, response);
+    }
+}
