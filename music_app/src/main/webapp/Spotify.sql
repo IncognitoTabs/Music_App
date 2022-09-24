8b8d2d9b594b276a20e075f095af615d1bc7ebb3
CaoLamBaoKhanh <khanhcaolambao@gmail.com>
2022-09-24 15:33:10 +0700

    fix csdl

--- music_app/src/main/webapp/Spotify.sql
+++ music_app/src/main/webapp/Spotify.sql
@@ -4,18 +4,18 @@ grant resource, connect, dba to c##spotify;
 
 create table album(
 	id_album varchar2(10) primary key,
-	name_albums varchar2(255),
+	name_albums nvarchar2(255),
 	id_singer varchar2(10),
-	genre_album varchar2(255),
+	genre_album nvarchar2(255),
 	artWorkPath varchar2(255),
-	postedBy_album varchar2(255),
-	show varchar2(255),
+	postedBy_album nvarchar2(255),
+	show nvarchar2(255),
 	postDay date
 )
 
 create table singer(
 	id_singer varchar2(10) primary key ,
-	name_singer varchar2(100),
+	name_singer nvarchar2(100),
 	picture_singer clob
 )
 create table song(
@@ -25,18 +25,18 @@ create table song(
     id_genre varchar2(10),
 	path_song varchar2(255),
 	listens number (10),
-	postBy_song varchar2(255),
-	postDay_song varchar2(255)
+	postBy_song nvarchar2(255),
+	postDay_song date
 )
 create table genres(
 	id_genre varchar2(10) primary key,
-	name_genre varchar2(255)
+	name_genre nvarchar2(255)
 )
 
 create table users(
 	id_user varchar2(10) primary key,
-	name_user varchar2(255),
-	name_account varchar2(255),
+	name_user nvarchar2(255),
+	name_account nvarchar2(255),
 	password varchar2(20),
 	email varchar2(255),
 	decentralization number(1),
@@ -54,86 +54,6 @@ FOREIGN KEY (id_singer) REFERENCES singer(id_singer);
 ALTER TABLE song 
 ADD CONSTRAINT fk_song_genre
 FOREIGN KEY (id_genre) REFERENCES genres(id_genre);
-
---Thêm d? li?u vào b?ng singer
-insert into singer(id_singer, name_singer)
-values ('01',N'Đàm Vĩnh Hưng');
-insert into singer(id_singer, name_singer)
-values ('02',N'Sơn Tùng M-TP');
-insert into singer(id_singer, name_singer)
-values ('03',N'Hà Anh Tuấn');
-insert into singer(id_singer, name_singer)
-values ('04',N'Bùi Anh Tuấn');
-insert into singer(id_singer, name_singer)
-values ('05',N'Phan Mạnh Quỳnh');
-insert into singer(id_singer, name_singer)
-values ('06',N'Lương Bích Hữu');
-insert into singer(id_singer, name_singer)
-values ('07',N'Uyên Linh');
-insert into singer(id_singer, name_singer)
-values ('08',N'Thu Minh');
-
---Thêm d? li?u vào b?ng genres
-insert into genres values ('01','Pop');
-insert into genres values ('02','Rock');
-insert into genres values ('03','Jazz');
-insert into genres values ('04','Opera');
-insert into genres values ('05','Country');
-insert into genres values ('06','RnB');
-insert into genres values ('07','Dance');
-insert into genres values ('08','Blues');
-insert into genres values ('09','Ballad');
-insert into genres values ('10','Accoustic');
-insert into genres values ('11','Bolero');
-
-insert into album (id_album, name_albums, id_singer)
-values ('01',n'Vol.2 Ây da Ây da','06');
-insert into album (id_album, name_albums, id_singer)
-values ('02','It is Not Over','06');
-insert into album (id_album, name_albums, id_singer)
-values ('03','Story of Time','06');
-insert into album (id_album, name_albums, id_singer)
-values ('04',N'Đức từng đoạn ruột','06');
-insert into album (id_album, name_albums, id_singer)
-values ('05',N'Mình cưới nhau nhé','06');
--------------------------------------------
-
-insert into users (id_user, name_user, name_account, password, email, decentralization, registration_date )
-values('ad1','admin','admin','admin','admin@gmail.com',1,'13-sep-22 ');
-
-insert into users (id_user, name_user, name_account, password, email, decentralization, registration_date )
-values('au1','Khanh','Khanh','Khanh','Khanh@gmail.com',0,'13-sep-22 ');
-insert into users (id_user, name_user, name_account, password, email, decentralization, registration_date )
-values('au2',N'Tài','Tai','Tai','Tai@gmail.com',0,'13-sep-22 ');
-insert into users (id_user, name_user, name_account, password, email, decentralization, registration_date )
-values('au3',N'Hi?p','Hiep','Hiep','Hiep@gmail.com',0,'13-sep-22 ');
-insert into users (id_user, name_user, name_account, password, email, decentralization, registration_date )
-values('au4',N'Nhân','Nhan','Nhan','Nhan@gmail.com',0,'13-sep-22 ');
-insert into users (id_user, name_user, name_account, password, email, decentralization, registration_date )
-values('au5',N'???c','Duoc','Duoc','Duoc@gmail.com',0,'13-sep-22 ');
-insert into users (id_user, name_user, name_account, password, email, decentralization, registration_date )
-values('au6',N'H?c','Hoc','Hoc','Hoc@gmail.com',0,'13-sep-22 ');
-insert into users (id_user, name_user, name_account, password, email, decentralization, registration_date )
-values('au7',N'??i','Dai','Dai','Dai@gmail.com',0,'13-sep-22 ');
-
-
-------------------------------------------
-insert into song (id_song, id_singer, id_album, id_genre)
-values ('lbh01','06','01','01');
----------------------------
---trigger
-CREATE OR REPLACE TRIGGER bi_album
-BEFORE INSERT ON album
-for each row
-enable
-declare 
-    v_user varchar2(10);
-begin
-    select user into v_user from dual;
-    dbms_output.put_line('you inserted a line by '||v_user);
-end;
-
-insert into album(id_album,name_albums,id_singer) values('07',N'Bước qua nhau','05');
 ------------
 
 
@@ -285,4 +205,187 @@ begin
 
 end;
 
+-------------------
+create table spotify_event_logon_logoff(
+    event_type varchar2(20),
+    logon_date date,
+    logon_time varchar2(15),
+    logof_date date,
+    logof_time varchar2(15)
+);
 
+
+create or replace trigger spotify_logon
+after logon on schema
+begin
+    insert into spotify_event_logon_logoff values(
+    ora_sysevent,
+    sysdate,
+    to_char(sysdate,'hh24:mi:ss'),
+    null,
+    null);
+    commit;
+
+end;
+--select * from spotify_event_logon_logoff;
+--disc;
+--conn c##spotify/spotify;
+--------
+--create or replace trigger spotify_logoff
+--before logoff on schema
+--begin
+--    insert into spotify_event_logon_logoff values(
+--    ora_sysevent,
+--    null,
+--    null,
+--    sysdate,
+--    to_char(sysdate,'hh24:mi:ss'));
+--    commit;
+--
+--end;
+--select * from spotify_event_logon_logoff;
+--disc;
+--conn c##spotify/spotify;
+
+
+---trigger auto-crement table users
+--step 1
+create sequence users_seq start with 1 increment by 1;
+--step 2
+CREATE OR REPLACE TRIGGER  bi_users
+  before insert on users             
+  for each row  
+begin   
+  if :NEW.id_user is null then 
+    select users_seq.nextval into :NEW.id_user from dual; 
+  end if; 
+end;
+---trigger auto-crement table song
+create sequence song_seq start with 10 increment by 1;
+
+CREATE OR REPLACE TRIGGER  bi_song_trg
+  before insert on song             
+  for each row  
+begin   
+  if :NEW.id_song is null then 
+    select song_seq.nextval into :NEW.id_song from dual; 
+  end if; 
+end;
+
+---trigger auto-crement table singer
+create sequence singer_seq start with 10 increment by 1;
+
+CREATE OR REPLACE TRIGGER  bi_singer_trg
+  before insert on singer             
+  for each row  
+begin   
+  if :NEW.id_singer is null then 
+    select singer_seq.nextval into :NEW.id_singer from dual; 
+  end if; 
+end;
+---trigger auto-crement table genres
+
+create sequence genres_seq start with 10 increment by 1;
+
+CREATE OR REPLACE TRIGGER  bi_genres_trg
+  before insert on genres            
+  for each row  
+begin   
+  if :NEW.id_genre is null then 
+    select genres_seq.nextval into :NEW.id_genre from dual; 
+  end if; 
+end;
+---trigger auto-crement table album
+create sequence album_seq start with 10 increment by 1;
+
+CREATE OR REPLACE TRIGGER  bi_album_trg
+  before insert on album            
+  for each row  
+begin   
+  if :NEW.id_album is null then 
+    select album_seq.nextval into :NEW.id_album from dual; 
+  end if; 
+end;
+
+--add data into table singer
+insert into singer(name_singer)
+values (N'Đàm Vĩnh Hưng');
+insert into singer(name_singer)
+values (N'Sơn Tùng M-TP');
+insert into singer(name_singer)
+values (N'Hà Anh Tuấn');
+insert into singer(name_singer)
+values (N'Bùi Anh Tuấn');
+insert into singer(name_singer)
+values (N'Phan Mạnh Quỳnh');
+insert into singer(name_singer)
+values (N'Lương Bích Hưu');
+insert into singer(name_singer)
+values (N'Uyên Linh');
+insert into singer(name_singer)
+values (N'Thu Minh');
+
+--add data into table genres
+insert into genres(name_genre) values ('Pop');
+insert into genres(name_genre) values ('Rock');
+insert into genres(name_genre) values ('Jazz');
+insert into genres(name_genre) values ('Opera');
+insert into genres(name_genre) values ('Country');
+insert into genres(name_genre) values ('RnB');
+insert into genres(name_genre) values ('Dance');
+insert into genres(name_genre) values ('Blues');
+insert into genres(name_genre) values ('Ballad');
+insert into genres(name_genre) values ('Accoustic');
+insert into genres(name_genre) values ('Bolero');
+
+insert into album ( name_albums, id_singer)
+values (n'Vol.2 Ây da Ây da','15');
+insert into album ( name_albums, id_singer)
+values ('It is Not Over','15');
+insert into album ( name_albums, id_singer)
+values ('Story of Time','15');
+insert into album ( name_albums, id_singer)
+values (N'Đứt từng đoạn ruột','15');
+insert into album (name_albums, id_singer)
+values (N'Mình cưới nhau nhé','15');
+-------------------------------------------
+
+insert into users ( name_user, name_account, password, email, decentralization, registration_date )
+values('admin','admin','admin','admin@gmail.com',1,'13-sep-22 ');
+
+insert into users ( name_user, name_account, password, email, decentralization, registration_date )
+values('Khanh','Khanh','Khanh','Khanh@gmail.com',0,'13-sep-22 ');
+insert into users ( name_user, name_account, password, email, decentralization, registration_date )
+values(N'Tai','Tai','Tai','Tai@gmail.com',0,'13-sep-22 ');
+insert into users ( name_user, name_account, password, email, decentralization, registration_date )
+values(N'Hiep','Hiep','Hiep','Hiep@gmail.com',0,'13-sep-22 ');
+insert into users ( name_user, name_account, password, email, decentralization, registration_date )
+values(N'Nhan','Nhan','Nhan','Nhan@gmail.com',0,'13-sep-22 ');
+insert into users ( name_user, name_account, password, email, decentralization, registration_date )
+values(N'Duoc','Duoc','Duoc','Duoc@gmail.com',0,'13-sep-22 ');
+insert into users ( name_user, name_account, password, email, decentralization, registration_date )
+values(N'Hoc','Hoc','Hoc','Hoc@gmail.com',0,'13-sep-22 ');
+insert into users ( name_user, name_account, password, email, decentralization, registration_date )
+values(N'Dai','Dai','Dai','Dai@gmail.com',0,'13-sep-22 ');
+
+insert into users ( name_user, name_account, password, email, decentralization, registration_date )
+values(N'Phung','Phung','Phung','Phung@gmail.com',0,'24-sep-22 ');
+
+
+------------------------------------------
+insert into song ( id_singer, id_album, id_genre,path_song,postday_song)
+values ('10','10','10','https://firebasestorage.googleapis.com/v0/b/fir-dc1e5.appspot.com/o/Mp3%2FLuongBichHuu%2FTaiNhacMienPhi.Net%20-%20Em%20D%E1%BB%ABng%20L%E1%BA%A1i.mp3?alt=media&token=aa5cfa33-81e9-4989-ab5a-405b4b4e8ee7','24-sep-22');
+---------------------------
+--trigger
+CREATE OR REPLACE TRIGGER bi_album
+BEFORE INSERT ON album
+for each row
+enable
+declare 
+    v_user varchar2(10);
+begin
+    select user into v_user from dual;
+    dbms_output.put_line('you inserted a line by '||v_user);
+end;
+
+insert into album(name_albums,id_singer) values(N'Buowsc qua nhau','10');
